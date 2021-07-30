a954be850a764a2e97c8270c3be1ca7beb504dd5
xieguigang <xie.guigang@gcmodeller.org>
2021-07-31 00:31:03 +0800

    translate to 3d surface?

---
+++ mesh/Modeller.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic.Imaging.Drawing2D.Math2D.MarchingSquares
+Imports Microsoft.VisualBasic.Imaging.Drawing3D
+
+Public NotInheritable Class Modeller
+
+    Private Sub New()
+    End Sub
+
+    Public Shared Function CreateMesh3D(sample As IEnumerable(Of MeasureData)) As Surface
+        Dim layers As GeneralPath() = ContourLayer.GetContours(sample).ToArray
+
+
+    End Function
+
+End Class
