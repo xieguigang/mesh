aefdc6b3c202a72136ef0e5b85b2f69318b658fb
xieguigang <xie.guigang@gcmodeller.org>
2021-07-31 00:52:22 +0800

    create mesh

--- mesh/Modeller.vb
+++ mesh/Modeller.vb
@@ -1,15 +1,54 @@
-﻿Imports Microsoft.VisualBasic.Imaging.Drawing2D.Math2D.MarchingSquares
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Math2D.MarchingSquares
 Imports Microsoft.VisualBasic.Imaging.Drawing3D
+Imports Microsoft.VisualBasic.Imaging.Math2D
+Imports Microsoft.VisualBasic.Linq
 
 Public NotInheritable Class Modeller
 
     Private Sub New()
     End Sub
 
-    Public Shared Function CreateMesh3D(sample As IEnumerable(Of MeasureData)) As Surface
+    Public Shared Iterator Function CreateMesh3D(sample As IEnumerable(Of MeasureData)) As IEnumerable(Of Surface)
         Dim layers As GeneralPath() = ContourLayer.GetContours(sample).ToArray
+        Dim base As GeneralPath = layers(Scan0)
 
+        For Each higher As GeneralPath In layers.Skip(1)
+            Dim basePoints As PointF() = base.GetPolygons.IteratesALL.ToArray
+            Dim higherPoints As PointF() = higher.GetPolygons.IteratesALL.ToArray
 
+            ' each point in higher layer query two nearest points
+            ' in base layer for create a triangle mesh
+            For Each polygon As PointF() In higher.GetPolygons
+                For Each point As PointF In polygon
+                    Dim twoPoints = basePoints.OrderBy(Function(d) d.Distance(point)).Take(2).ToArray
+
+                    Yield New Surface With {
+                        .vertices = {
+                            New Point3D(point.X, point.Y, higher.level),
+                            New Point3D(twoPoints(0).X, twoPoints(0).Y, base.level),
+                            New Point3D(twoPoints(1).X, twoPoints(1).Y, base.level)
+                        }
+                    }
+                Next
+            Next
+
+            For Each polygon As PointF() In base.GetPolygons
+                For Each point As PointF In polygon
+                    Dim twoPoints = higherPoints.OrderBy(Function(d) d.Distance(point)).Take(2).ToArray
+
+                    Yield New Surface With {
+                        .vertices = {
+                            New Point3D(point.X, point.Y, base.level),
+                            New Point3D(twoPoints(0).X, twoPoints(0).Y, higher.level),
+                            New Point3D(twoPoints(1).X, twoPoints(1).Y, higher.level)
+                        }
+                    }
+                Next
+            Next
+
+            base = higher
+        Next
     End Function
 
 End Class
--- mesh/test/Program.vb
+++ mesh/test/Program.vb
@@ -1,6 +1,24 @@
-﻿Public Module Program
+﻿Imports mesh
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Math2D.MarchingSquares
+
+Public Module Program
 
     Sub Main()
+        Dim data As MeasureData() = {
+            New MeasureData(1, 213, 10),
+            New MeasureData(10, 19, 100),
+            New MeasureData(122, 661, 1000),
+            New MeasureData(441, 371, 20),
+            New MeasureData(751, 81, 56),
+            New MeasureData(201, 691, 1),
+            New MeasureData(2231, 101, 888),
+            New MeasureData(31, 1561, 230),
+            New MeasureData(1555, 1111, 10000),
+            New MeasureData(91, 132, 770)
+        }
 
+        Dim meshData = Modeller.CreateMesh3D(data).ToArray
+
+        Pause()
     End Sub
 End Module
