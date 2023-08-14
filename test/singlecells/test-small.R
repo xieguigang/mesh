71ddf384d2e3b94ce98e1dc8720c2037d2e218ea
guigang xie <xie.guigang@gcmodeller.org>
2023-08-14 13:58:02 +0800

    config for run small demo test

--- test/singlecells/test-small.R
+++ test/singlecells/test-small.R
@@ -9,10 +9,10 @@ let mesh = mesh(
     mass.range = [50, 2000], 
     feature.size = 100, 
     mzdiff = 0.005);
-let raster = as.raster(readImage(`${@dir}/../../docs/docs\single-cells-have-their-own-defenses-against-pathogens3.jpg`)); 
+let raster = as.raster(readImage(`${@dir}/../../docs/Visualize-Metabolic-Process-at-the-Single-Cell-Level.png`)); 
 
 samples.raster(mesh, raster);
 
 let pack = mesh::expr1(mesh, mzpack = TRUE, spatial = TRUE);
 
-write.mzPack(pack, file = `${@dir}/demo_singlecells_small.mzPack`, version = 2);
+write.mzPack(pack, file = `${@dir}/demo_singlecells.mzPack`, version = 2);
