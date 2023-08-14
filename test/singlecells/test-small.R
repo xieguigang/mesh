f1c933628033d324fca5b71d96dfedba22c2c6b3
guigang xie <xie.guigang@gcmodeller.org>
2023-08-14 14:04:29 +0800

    create raster demo

--- test/singlecells/test-small.R
+++ test/singlecells/test-small.R
@@ -2,14 +2,17 @@ require(mesh);
 require(GCModeller);
 require(mzkit);
 require(graphics);
+require(graphics2D);
 
 imports "mzweb" from "mzkit";
+
+setwd(@dir);
 
 let mesh = mesh(
     mass.range = [50, 2000], 
     feature.size = 100, 
     mzdiff = 0.005);
-let raster = as.raster(readImage(`${@dir}/../../docs\single-cells-have-their-own-defenses-against-pathogens3.jpg`)); 
+let raster = as.raster(readImage(`../../docs\single-cells-have-their-own-defenses-against-pathogens3.jpg`)); 
 
 bitmap(file = "./raster1.png", size = [1920, 1080]);
 rasterHeatmap(raster);
@@ -19,4 +22,4 @@ samples.raster(mesh, raster);
 
 let pack = mesh::expr1(mesh, mzpack = TRUE, spatial = TRUE);
 
-write.mzPack(pack, file = `${@dir}/demo_singlecells.mzPack`, version = 2);
+write.mzPack(pack, file = `demo_singlecells.mzPack`, version = 2);
