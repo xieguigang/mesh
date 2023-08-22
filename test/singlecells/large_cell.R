f19ebfa08531ab2800fdab16f6217750bc541161
guigang xie <xie.guigang@gcmodeller.org>
2023-08-22 17:42:15 +0800

    why divid by zero?

---
+++ test/singlecells/large_cell.R
@@ -0,0 +1,46 @@
+require(mesh);
+require(GCModeller);
+require(mzkit);
+require(graphics);
+require(graphics2D);
+
+imports "mzweb" from "mzkit";
+imports "clustering" from "MLkit";
+imports "dataset" from "MLkit";
+
+setwd(@dir);
+
+let mesh = mesh(
+    mass.range = [50, 2000], 
+    feature.size = 100, 
+    mzdiff = 0.005);
+let raster = as.raster(readImage(`../../docs/cell.jpg`)); 
+
+let labels = raster_vec(raster);
+# let gmm = clustering::gmm(labels, components = 6);
+# let gauss = gmm.predict_proba(gmm);
+
+print(labels);
+
+labels = q_factors(labels, levels = 9);
+
+print(labels);
+
+# labels = gmm.predict(gmm);
+
+# print(gauss, max.print = 6);
+# print(labels);
+# print("unique labels:");
+# print(unique(labels));
+
+# write.csv(gauss, file = "./gauss.csv", row.names = TRUE);
+
+bitmap(file = "./raster_cell1.png", size = [1920, 1920]);
+rasterHeatmap(raster);
+dev.off();
+
+samples.raster(mesh, raster, label = labels);
+
+let pack = mesh::expr1(mesh, mzpack = TRUE, spatial = TRUE);
+
+write.mzPack(pack, file = `singlecell.mzPack`, version = 2);
