b150ba46467b2d8579a1106a02c7fabb9616b7d1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-09 11:19:49 +0800

    Update test.R

--- test/singlecells/test.R
+++ test/singlecells/test.R
@@ -12,7 +12,7 @@ setwd(@dir);
 
 let mesh = mesh(
     mass.range = [50, 2000], 
-    feature.size = 125, 
+    feature.size = 1000, 
     mzdiff = 0.005);
 let raster = as.raster(readImage(`../../docs\Visualize-Metabolic-Process-at-the-Single-Cell-Level.png`)); 
 
