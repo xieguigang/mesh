4c34f5d909eaf60f98c204bca1ae5174e4e2aa87
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-09-20 22:05:54 +0800

    TrIQ trim data

--- src/mesh/Rscript.vb
+++ src/mesh/Rscript.vb
@@ -5,6 +5,7 @@ Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
 Imports BioNovoGene.BioDeep.Chemoinformatics
 Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors.Scaler
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
@@ -147,6 +148,7 @@ Public Module Rscript
                                   Optional label As Object = Nothing,
                                   Optional kernel_cutoff As Double = 0.0001,
                                   Optional linear_kernel As Boolean = False,
+                                  Optional TrIQ As Double = 0.85,
                                   Optional env As Environment = Nothing) As Object
 
         Dim pixels As PixelData() = raster.GetRasterData.ToArray
@@ -154,7 +156,9 @@ Public Module Rscript
         Dim y As Integer() = pixels.Select(Function(p) p.Y).ToArray
         Dim kernels As Vector = pixels.Select(Function(p) p.Scale).AsVector
         Dim labels As String() = CLRVector.asCharacter(label)
+        Dim trim As Double = kernels.FindThreshold(q:=TrIQ)
 
+        kernels(kernels > trim) = Vector.Scalar(trim)
         kernels = kernels / kernels.Max
         kernels(kernels < kernel_cutoff) = Vector.Zero
 
