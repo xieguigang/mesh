5f0b4631cdf6fc14d27918e0359a34895b2e72fa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-10-14 12:43:42 +0800

    make improvements of the MS1 scan data assembly

--- src/mesh/MSData.vb
+++ src/mesh/MSData.vb
@@ -9,53 +9,45 @@ Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
 Module MsData
 
     Public Function PopulateMs1Scan(sampleId As String, t As Double, q As Double, mz As Vector, sampleinfo As SampleInfo(), sample As DataFrameRow()) As ScanMS1
+        Dim expression As Vector = sample.Sum
 
     End Function
 
-    <Extension>
-    Public Function PopulateMs1Scan(sample As DataFrameRow,
-                                    t As Double,
-                                    q As Double,
-                                    spatial As Boolean,
-                                    mz As Vector,
-                                    sampleinfo As Dictionary(Of String, SampleInfo)) As ScanMS1
-
-        Dim expression As New Vector(sample.experiments)
+    Private Function AssembleMSScanData(scan_id As String, geneId As String, t As Double, q As Double, spatial As Boolean,
+                                        mz As Vector,
+                                        expression As Vector,
+                                        sample_data As SampleInfo) As ScanMS1
         Dim cut As Double = 0
 
         If q > 0 Then
-            cut = sample.experiments.GKQuantile.Query(q)
+            cut = expression.GKQuantile.Query(q)
         End If
 
         Dim i = expression > cut
         Dim mzi = mz(i)
         Dim into = expression(i)
-        Dim scan_id As String
-        Dim sample_data As SampleInfo = Nothing
-        Dim s1 As ScanMS1
 
         If mzi.Length = 0 Then
             Return Nothing
-        End If
-
-        If sampleinfo.ContainsKey(sample.geneID) Then
-            sample_data = sampleinfo(sample.geneID)
-            scan_id = sample_data.sample_name
         Else
-            scan_id = sample.geneID
+            Dim totalIons = into.Sum.ToString("G3")
+            Dim baseInto = into.Max.ToString("G3")
+            Dim basePeak = mzi(which.Max(into)).ToString("F3")
+
+            scan_id = $"[MS1] {scan_id}, {mzi.Length} ions; total_ions={totalIons}, basePeak={baseInto}, basePeak_m/z={basePeak}"
         End If
 
-        s1 = New ScanMS1 With {
+        Dim s1 As New ScanMS1 With {
             .mz = mzi.ToArray,
             .BPC = into.Max,
             .into = into.ToArray,
             .rt = t,
             .TIC = expression.Sum,
-            .scan_id = $"[MS1] {scan_id}, { .mz.Length} ions; total_ions={ .into.Sum.ToString("G3")}, basePeak={ .into.Max.ToString("G3")}, basePeak_m/z={ .mz(which.Max(.into)).ToString("F3")}"
+            .scan_id = scan_id
         }
 
         If spatial Then
-            Dim xyz As String() = sample.geneID.Split(","c)
+            Dim xyz As String() = geneId.Split(","c)
 
             s1.meta = New Dictionary(Of String, String) From {
                 {"x", xyz(0)},
@@ -75,5 +67,27 @@ Module MsData
         End If
 
         Return s1
+    End Function
+
+    <Extension>
+    Public Function PopulateMs1Scan(sample As DataFrameRow,
+                                    t As Double,
+                                    q As Double,
+                                    spatial As Boolean,
+                                    mz As Vector,
+                                    sampleinfo As Dictionary(Of String, SampleInfo)) As ScanMS1
+
+        Dim expression As New Vector(sample.experiments)
+        Dim sample_data As SampleInfo = Nothing
+        Dim scan_id As String
+
+        If sampleinfo.ContainsKey(sample.geneID) Then
+            sample_data = sampleinfo(sample.geneID)
+            scan_id = sample_data.sample_name
+        Else
+            scan_id = sample.geneID
+        End If
+
+        Return AssembleMSScanData(scan_id, sample.geneID, t, q, spatial, mz, expression, sample_data)
     End Function
 End Module
