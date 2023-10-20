9e4ad9b2cce674e4809869195ef475e4a3b2b1cd
guigang xie <xie.guigang@gcmodeller.org>
2023-10-20 14:40:48 +0800

    fix of the number count of the sample id mis-matched

--- src/mesh/Creator/Generator.vb
+++ src/mesh/Creator/Generator.vb
@@ -35,19 +35,23 @@ Public Class Generator
     ''' 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Function GetExpressionMatrix() As Matrix
+        Dim sample_info As New List(Of String)
+        Dim expr_mat As DataFrameRow() = GetIonExpressions(sample_info).ToArray
+
         Return New Matrix With {
-            .sampleID = sample_groups _
-                .Select(Function(group) group.Value.Select(Function(sample) sample.ID)) _
-                .IteratesALL _
-                .ToArray,
+            .sampleID = sample_info.ToArray,
             .tag = "mesh_generator",
-            .expression = GetIonExpressions.ToArray
+            .expression = expr_mat
         }
     End Function
 
-    Private Iterator Function GetIonExpressions() As IEnumerable(Of DataFrameRow)
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sample_info">pull the sample id list from this populator function</param>
+    ''' <returns></returns>
+    Private Iterator Function GetIonExpressions(sample_info As List(Of String)) As IEnumerable(Of DataFrameRow)
         Dim ions As Double() = Me.ions.Clear().CreateIonFeatures().Shuffles.ToArray
-        Dim sample_info As New List(Of (name As String, SampleInfo()))
         Dim sample_data As New List(Of Double())
         Dim zero As Vector = Vector.Zero
 
@@ -57,7 +61,7 @@ Public Class Generator
             Call VBDebugger.EchoLine($"    -> {sample_group.Value.Length} sample files...")
             Call VBDebugger.EchoLine("")
 
-            sample_info.Add((sample_group.Key, sample_group.Value))
+            sample_info.AddRange(sample_group.Value.Select(Function(sample) sample.ID))
 
             For Each v As Vector In SampleMatrix(sample_group.Value)
                 v(v.IsNaN) = zero
@@ -72,7 +76,7 @@ Public Class Generator
                 Call VBDebugger.EchoLine($"    -> {cal_group.Count} sample files...")
                 Call VBDebugger.EchoLine("")
 
-                Call sample_info.Add((cal_group.Key, cal_group.ToArray))
+                Call sample_info.AddRange(cal_group.Select(Function(sample) sample.ID))
 
                 Dim maxinto As Double = Val(cal_group.First.color) * args.intensity_max
 
