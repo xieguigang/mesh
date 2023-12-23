db3a945c66c6020f2a55e3e69f597d023cd28728
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-23 12:23:36 +0800

    Update dump_frames.R

--- test/dump_frames.R
+++ test/dump_frames.R
@@ -2,23 +2,40 @@ imports "dataset" from "MLkit";
 
 require(JSON);
 require(CNN);
+require(graphics);
 
 setwd("F:/");
 
 let dataset = read.sample_set(file = "./spectrum.dat");
+let metadata = "metadata.json"
+|> readText()
+|> JSON::json_decode()
+;
+
 dataset = sort_samples(dataset, order_id = 5, desc = TRUE);
 
 let imgs = list.files("./AtlasSlices", pattern = "*.jpg");
 let global = [0, 1];
+let decoder = CNN::cnn("./spectrum.cnn");
 
 print(imgs);
+str(metadata);
 
 for(path in imgs) {
-    let raster = as.raster(readImage(path)); 
-    let v = raster_vec(ratser);
-    let minmax = range(v);
+    let raster = as.data.frame(as.raster(readImage(path))); 
+    raster = raster[raster$scale > 0, ];
+    let i = as.integer(length(dataset) * raster$scale);
+    let seeds = dataset[i];
+    let x = raster$x;
+    let y = raster$y;
+    let total = get_feature(seeds, );
+    let spectrum_data = decoder(seeds, class_labels = as.character(metadata$ions),
+                                 is_generative = TRUE);
 
-    print(minmax);
+    print(raster, max.print = 6);
+    print(i);
+    str(spectrum_data);
 
     stop();
 }
+
