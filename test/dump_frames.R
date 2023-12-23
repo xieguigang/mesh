578af39cfbfd15c29d150c3ddd361fe0c03c9b33
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-23 12:52:19 +0800

    Update dump_frames.R

--- test/dump_frames.R
+++ test/dump_frames.R
@@ -3,6 +3,9 @@ imports "dataset" from "MLkit";
 require(JSON);
 require(CNN);
 require(graphics);
+require(mzkit);
+
+imports ["SingleCells", "MSI"] from "mzkit";
 
 setwd("F:/");
 
@@ -17,6 +20,7 @@ dataset = sort_samples(dataset, order_id = 5, desc = TRUE);
 let imgs = list.files("./AtlasSlices", pattern = "*.jpg");
 let global = [0, 1];
 let decoder = CNN::cnn("./spectrum.cnn");
+let res = [ 528, 320 ];
 
 print(imgs);
 str(metadata);
@@ -28,13 +32,25 @@ for(path in imgs) {
     let seeds = dataset[i];
     let x = raster$x;
     let y = raster$y;
-    let total = get_feature(seeds, );
+    let total = get_feature(seeds, 5);
     let spectrum_data = decoder(seeds, class_labels = as.character(metadata$ions),
                                  is_generative = TRUE);
 
+    rownames(spectrum_data) = `${x},${y}`;
+
     print(raster, max.print = 6);
     print(i);
+    print(total);
     str(spectrum_data);
+
+    let msi_data = t(spectrum_data);
+     msi_data = MSI::scale(msi_data, total);
+    msi_data = t(msi_data);
+    let save_export = `${dirname(path)}/${basename(path)}.mzPack`;
+
+    let rawdata = MSI::pack_matrix(file = msi_data, res);
+
+    write.mzPack(rawdata, file = save_export);
 
     stop();
 }
