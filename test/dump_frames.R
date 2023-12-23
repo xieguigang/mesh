7b34e81067db277f3b50314eb9c8e2d8f4840072
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-23 11:47:25 +0800

    Update dump_frames.R

--- test/dump_frames.R
+++ test/dump_frames.R
@@ -9,9 +9,16 @@ let dataset = read.sample_set(file = "./spectrum.dat");
 dataset = sort_samples(dataset, order_id = 5, desc = TRUE);
 
 let imgs = list.files("./AtlasSlices", pattern = "*.jpg");
+let global = [0, 1];
 
 print(imgs);
 
 for(path in imgs) {
-    
+    let raster = as.raster(readImage(path)); 
+    let v = raster_vec(ratser);
+    let minmax = range(v);
+
+    print(minmax);
+
+    stop();
 }
